7025b82c296e7f98f2b2de6abb6efe4c2aad0d1d
Anthony Nardiello <nardielloam@gmail.com>
2023-02-14 18:58:40 -0700

    adds the checkpoint 5 completion, and the day 9 playground

---
+++ Checkpoint5.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+let solve = {(_: [Int]) in
+    luckyNumbers.filter{ !$0.isMultiple(of: 2)}
+        .sorted()
+        .map{print("\($0) is a lucky number")}
+}
+
+solve(luckyNumbers)

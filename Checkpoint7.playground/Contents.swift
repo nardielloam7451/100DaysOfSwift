13e2115723e170b3ecd92f8bf85bfac3a41b813b
Anthony Nardiello <nardielloam@gmail.com>
2023-02-17 15:19:46 -0700

    finishes the work for day 12, by adding the implementation to checkpoint7

--- Checkpoint7.playground/Contents.swift
+++ Checkpoint7.playground/Contents.swift
@@ -1,3 +1,55 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+class Animal{
+    var legs: Int
+    
+    init(numLegs: Int){
+        self.legs = numLegs
+    }
+}
+
+class Dog: Animal{
+    func speak(){
+        print("BARK!")
+    }
+}
+
+class Cat: Animal{
+    var isTame: Bool
+    
+    init(isTame: Bool, numLegs: Int){
+        self.isTame = isTame
+        super.init(numLegs: numLegs)
+    }
+    
+    func speak(){
+        print("Meow")
+    }
+}
+
+class Corgi: Dog{
+    override func speak(){
+        print("Yip")
+    }
+}
+
+class Poodle: Dog{
+    override func speak(){
+        print("Growl")
+    }
+}
+
+class Persian: Cat{
+    override func speak(){
+        print("Screech")
+    }
+}
+
+class Lion: Cat{
+    override func speak(){
+        print("Roar")
+    }
+}
+
+let leo = Lion(isTame: false, numLegs: 4)
+leo.speak()

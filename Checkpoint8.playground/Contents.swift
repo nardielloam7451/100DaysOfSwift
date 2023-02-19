83f8529499d43bf43e625c00f13c67fd95b26381
Anthony Nardiello <nardielloam@gmail.com>
2023-02-19 13:20:53 -0700

    implements the details on checkpoint 8, now bringing myself one step closer to my goal

--- Checkpoint8.playground/Contents.swift
+++ Checkpoint8.playground/Contents.swift
@@ -1,3 +1,33 @@
 import Cocoa
 
-protocol 
+protocol Building{
+    var rooms: Int { get set }
+    var cost: Int { get set }
+    var realtor: String { get }
+    
+    func salesSummary(soldTo: String)
+}
+
+struct House: Building{
+    var rooms = 8
+    var cost = 250_000
+    let realtor = "Jake Masters"
+    
+    func salesSummary(soldTo: String){
+        print("\(realtor) sold this lovely house with \(rooms) to \(soldTo) for \(cost)")
+    }
+}
+
+struct Office: Building{
+    var rooms = 250
+    var cost = 1_000_000
+    let realtor = "Donna Sue"
+    
+    func salesSummary(soldTo: String){
+        print("\(realtor) sold this office building to \(soldTo) for \(cost), who is planning to use it for his startup")
+    }
+}
+
+
+let house = House()
+house.salesSummary(soldTo: "Jennifer Nardiello")

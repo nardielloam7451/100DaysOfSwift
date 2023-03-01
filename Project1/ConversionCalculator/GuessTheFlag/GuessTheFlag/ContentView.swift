678e16b478c70e8ad9a980482c8d9270711efa73
Anthony Nardiello <nardielloam@gmail.com>
2023-03-01 15:01:04 -0700

    adds the files necessary for the day 20 swift challenge

---
+++ Project1/ConversionCalculator/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Anthony Nardiello on 3/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

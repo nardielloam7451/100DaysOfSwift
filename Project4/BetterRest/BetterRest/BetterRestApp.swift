4e8c01b58d54d91526cd1588a972554b267510dd
Anthony Nardiello <nardielloam@gmail.com>
2023-03-09 23:20:22 -0700

    adds the files from day 26, which involves the beginning of project 4

---
+++ Project4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Anthony Nardiello on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
